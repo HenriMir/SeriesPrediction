0ed7bdc7e74a854b544f93133b8aac4c6cee6d6b
henri <henri.mirande@polynom.io>
2019-01-03 13:09:59 +0100

    peaufiner la fonction load_csv pour que les clés de sortie soient toujours le smêmes quellle que soit le csv d entree

--- load_data.py
+++ load_data.py
@@ -43,24 +43,6 @@ def load_csv(csv_name, names_dict=None):
     return df
 
 
-def load_range(start_date,
-               end_date,
-               data_names,
-               csv_path=data_path+"BTC_EUR GDAX - Données Historiques .csv"):
-    """
-
-    :param start_date: str: "10/09/2018"
-    :param end_date: idem
-    :param data_names: list de str: ['Date', 'Dernier', 'Ouv.', 'Haut', '
-    :return: array numpy de shape (T,n) avec T l'intervalle de temps et n la taille
-             de data_names. array de string
-    """
-
-    data = pd.read_csv(csv_path, encoding='utf-8').set_index("Date")
-    data = data.loc[end_date:start_date]
-    data = data[data_names][::-1]  # le -1 pour lire dans l'autre sens sur la date
-    return data
-
 
 if __name__ == '__main__':
 
